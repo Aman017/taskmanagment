6187e06537e95ac028c046547e5c0f77d0c6868d
Aman Malik <amanmalik0135@gmail.com>
2026-03-30 18:27:33 +0530

    server

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,8 @@ const allowedOrigins = [
   'http://localhost:5173',
   'http://localhost:5174',
   'http://127.0.0.1:3000',
-  'http://127.0.0.1:5173'
+  'http://127.0.0.1:5173',
+  'https://taskmanagment-umber.vercel.app'
 ];
 
 app.use(cors({
